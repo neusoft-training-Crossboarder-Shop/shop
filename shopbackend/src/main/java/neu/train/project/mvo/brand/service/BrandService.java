ca3a2f95c94820f231fbf16db9820c2c5bb7eda0
EvilicLufas <cestlavenir@outlook.com>
2020-07-08 00:03:32 +0800

    update the backend of company and brand

--- shopbackend/src/main/java/neu/train/project/mvo/brand/service/BrandService.java
+++
@@ -1,9 +0,0 @@
-package neu.train.project.mvo.brand.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class BrandService {
-
-}
