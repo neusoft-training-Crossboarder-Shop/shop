283587b13b7ad07197e01252a4204e0c53ca8ae5
EvilicLufas <cestlavenir@outlook.com>
2020-07-11 04:01:26 +0800

    update the whole frontend of mvo and connect it to the backend (while backend encountered the mappers problem in merging with bvo branch)

--- shop-ui/src/views/mvo/manufacturer/manufacturer.vue
+++ shop-ui/src/views/mvo/manufacturer/manufacturer.vue
@@ -1,50 +1,60 @@
+
+<!-- @Author: 高歌-->
+<!-- @Id: 20175045-->
+<!-- @Github : EvilicLufas-->
+
 <template>
   <div class="app-container">
 
     <!-- 查询和其他操作 -->
     <div class="filter-container">
-      <el-input v-model="listQuery.id" clearable class="filter-item" style="width: 200px;" placeholder="Enter the ID of brand"/>
-      <el-input v-model="listQuery.name" clearable class="filter-item" style="width: 200px;" placeholder="Enter the name of brand"/>
-      <el-button v-permission="['GET /admin/oldbrand/list']" class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">Search</el-button>
-      <el-button v-permission="['POST /admin/oldbrand/create']" class="filter-item" type="success" icon="el-icon-edit" @click="handleCreate">Add</el-button>
+      <el-input v-model="listQuery.id" clearable class="filter-item" style="width: 200px;" placeholder="Enter the ID of manufacturer"></el-input>
+      <el-input v-model="listQuery.name" clearable class="filter-item" style="width: 200px;" placeholder="Enter the name of manufacturer"></el-input>
+      <el-button  class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">Search</el-button>
+      <el-button  class="filter-item" type="success" icon="el-icon-edit" @click="handleCreate">Add</el-button>
+      <!--      <el-button v-permission="['GET /mvo/manufacturer/list']" class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">Search</el-button>-->
+      <!--      <el-button v-permission="['POST /mvo/manufacturer/create']" class="filter-item" type="success" icon="el-icon-edit" @click="handleCreate">Add</el-button>-->
       <el-button :loading="downloadLoading" class="filter-item" type="info" icon="el-icon-download" @click="handleDownload">Export</el-button>
     </div>
 
     <!-- 查询结果 -->
     <el-table v-loading="listLoading" :data="list" element-loading-text="Searching now..." border fit highlight-current-row>
 
-      <el-table-column align="center" label="Brand ID" prop="brd_id"/>
-      <el-table-column align="center" label="Company ID" prop="man_id"/>
+      <el-table-column align="center" label="manId" prop="manId"></el-table-column>
+      <el-table-column align="center" label="sysUserId" prop="sysUserId"></el-table-column>
 
-      <el-table-column align="center" label="Name_CHN" prop="name_en"/>
-      <el-table-column align="center" label="Name_EN" prop="name_chn"/>
+      <el-table-column align="center" label="nameEn" prop="nameEn"></el-table-column>
+      <el-table-column align="center" label="nameCn" prop="nameCn"></el-table-column>
+      <el-table-column align="center" label="picUrl" prop="picUrl"></el-table-column>
 
-      <el-table-column align="center" property="img_id" label="img_id">
+      <el-table-column align="center" property="picUrl" label="picUrl">
         <template slot-scope="scope">
-          <img v-if="scope.row.img_id" :src="scope.row.img_id" width="80">
+          <img v-if="scope.row.picUrl" :src="scope.row.picUrl" width="80"  alt="">
         </template>
       </el-table-column>
 
-<!--      <el-table-column align="center" min-width="400px" label="Desc" prop="desc"/>-->
+      <el-table-column align="center" label="gmcReportType" prop="gmcReportType"></el-table-column>
 
-      <el-table-column align="center" label="Creater" prop="created_by"/>
+      <el-table-column align="center" label="gmcReportUrl" prop="gmcReportUrl"></el-table-column>
 
+      <el-table-column align="center" label="Create_date" prop="createTime"></el-table-column>
 
-      <el-table-column align="center" label="Create_date" prop="create_time"/>
+      <el-table-column align="center" label="Updater" prop="lastUpdateBy"></el-table-column>
 
-      <el-table-column align="center" label="Updater" prop="last_update_by"/>
+      <el-table-column align="center" label="Update_date" prop="lastUpdateTime"></el-table-column>
+<!--      <el-table-column align="center" label="Remark" prop="callCnt"></el-table-column>-->
 
-      <el-table-column align="center" label="Update_date" prop="last_update_time"/>
-      <el-table-column align="center" label="Remark" prop="call_cnt"/>
+<!--      <el-table-column align="center" label="State" prop="stsCd"></el-table-column>-->
+<!--      <el-table-column align="center" label="Deleted" prop="deleted"></el-table-column>-->
 
-      <el-table-column align="center" label="State" prop="sts_cd"/>
-      <el-table-column align="center" label="Deleted" prop="deleted"/>
-
+      <el-table-column align="center" min-width="400px" label="desc" prop="manDesc"></el-table-column>
 
       <el-table-column align="center" label="Operation" width="200" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button v-permission="['POST /admin/oldbrand/update']" type="primary" size="mini" @click="handleUpdate(scope.row)">Edit</el-button>
-          <el-button v-permission="['POST /admin/oldbrand/delete']" type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+          <el-button  type="primary" size="middle" @click="handleUpdate(scope.row)">Edit</el-button>
+          <el-button  type="danger" size="middle" @click="handleDelete(scope.row)">Delete</el-button>
+          <!--          <el-button v-permission="['POST /mvo/manufacturer/update']" type="primary" size="mini" @click="handleUpdate(scope.row)">Edit</el-button>-->
+          <!--          <el-button v-permission="['POST /mvo/manufacturer/delete']" type="danger" size="mini" @click="handleDelete(scope.row)">Delete</el-button>-->
         </template>
       </el-table-column>
     </el-table>
@@ -56,24 +66,25 @@
     <!-- 添加或修改对话框 -->
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
       <el-form ref="dataForm" :rules="rules" :model="dataForm" status-icon label-position="left" label-width="180px" style="width: 800px; margin-left:50px;">
-        <el-form-item label="Name (CHN)" prop="name_en">
-          <el-input v-model="dataForm.name"/>
+        <el-form-item label="Name (CHN)" prop="nameEn">
+          <el-input v-model="dataForm.nameCn"></el-input>
         </el-form-item>
-        <el-form-item label="Name (EN)" prop="name_chn">
-          <el-input v-model="dataForm.name"/>
+        <el-form-item label="Name (EN)" prop="nameCn">
+          <el-input v-model="dataForm.nameEn"></el-input>
         </el-form-item>
 
-        <el-form-item label="Description" prop="simpleDesc">
+        <el-form-item label="Description" prop="manDesc">
 
           <el-card style="height: 700px;width:620px">
-            <quill-editor v-model="content" ref="myQuillEditor" style="height: 580px;width : 580px" :options="editorOption">
+            <!--            <quill-editor v-model="dataForm.desc" ref="myQuillEditor" style="height: 580px;width : 580px" :options="editorOption">-->
+            <quill-editor v-model="dataForm.manDesc" ref="myQuillEditor" style="height: 580px;width : 580px" >
             </quill-editor>
           </el-card>
 
         </el-form-item>
 
 
-        <el-form-item label="Company_img" prop="img_id">
+        <el-form-item label="Manufacturer_img" prop="picUrl">
           <el-upload
             :headers="headers"
             :action="uploadPath"
@@ -81,49 +92,23 @@
             :on-success="uploadPicUrl"
             class="avatar-uploader"
             accept=".jpg,.jpeg,.png,.gif">
-            <img v-if="dataForm.img_id" :src="dataForm.img_id" class="avatar">
-            <i v-else class="el-icon-plus avatar-uploader-icon"/>
+            <img v-if="dataForm.picUrl" :src="dataForm.picUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
         </el-form-item>
 
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false" type="danger">Cancel</el-button>
-        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">Confirm</el-button>
+        <el-button v-if="dialogStatus ==='create'" type="primary" @click="createData">Confirm</el-button>
         <el-button v-else  type="success" @click="updateData">Confirm</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
-<style>
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #20a0ff;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 120px;
-    height: 120px;
-    line-height: 120px;
-    text-align: center;
-  }
-  .avatar {
-    width: 145px;
-    height: 145px;
-    display: block;
-  }
-</style>
-
 <script>
-  import { listBrand, createBrand, updateBrand, deleteBrand } from '@/api/mvo/oldbrand'
+  import { listManufacturer, createManufacturer, updateManufacturer, deleteManufacturer } from '@/api/mvo/manufacturer'
   import { uploadPath } from '@/api/mvo/storage'
   import { getToken } from '@/utils/auth'
   import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
@@ -135,7 +120,7 @@
   import 'quill/dist/quill.bubble.css'
 
   export default {
-    name: 'brand',
+    name: "manufacturer",
     components: { Pagination, quillEditor },
     data() {
       return {
@@ -146,17 +131,26 @@
         listQuery: {
           page: 1,
           limit: 20,
-          id: undefined,
-          name: undefined,
+          manId: 'man_id',
+          nameCn: 'name_en',
           sort: 'create_time',
-          order: 'brd_id'
+          order: 'Desc' //order by create_time desc  mysql 语句
         },
         dataForm: {
-          id: undefined,
-          name: '',
-          desc: '',
-          floorPrice: undefined,
-          img_id: undefined
+          manId: '',
+          manId: '',
+          nameEn: '',
+          nameCn: '',
+          picUrl: '',
+          createdBy: '',
+          createTime: '',
+          lastUpdateBy: '',
+          lastUpdateTime: '',
+          callCnt: '',
+          deleted: '',
+          manDesc: ''
+          // name: '',
+          // desc: '',
         },
         dialogFormVisible: false,
         dialogStatus: '',
@@ -185,15 +179,15 @@
     methods: {
       getList() {
         this.listLoading = true
-        listBrand(this.listQuery)
+        listManufacturer(this.listQuery)
           .then(response => {
-            this.list = response.data.data.list
-            this.total = response.data.data.total
+            this.list = response.data.list;
+            this.total = response.data.total;
             this.listLoading = false
           })
           .catch(() => {
-            this.list = []
-            this.total = 0
+            this.list = [];
+            this.total = 0;
             this.listLoading = false
           })
       },
@@ -203,11 +197,18 @@
       },
       resetForm() {
         this.dataForm = {
-          id: undefined,
-          name: '',
-          desc: '',
-          floorPrice: undefined,
-          img_id: undefined
+          manId: undefined,
+          sysUserId: undefined,
+          nameEn: undefined,
+          nameCn: undefined,
+          picUrl: undefined,
+          createdBy: undefined,
+          createTime: undefined,
+          lastUpdateBy: undefined,
+          lastUpdateTime: undefined,
+          callCnt: undefined,
+          deleted: undefined,
+          manDesc: undefined
         }
       },
       handleCreate() {
@@ -219,12 +220,12 @@
         })
       },
       uploadPicUrl: function(response) {
-        this.dataForm.img_id = response.data.url
+        this.dataForm.picUrl = response.data.url
       },
       createData() {
         this.$refs['dataForm'].validate(valid => {
           if (valid) {
-            createBrand(this.dataForm)
+            createManufacturer(this.dataForm)
               .then(response => {
                 this.list.unshift(response.data.data)
                 this.dialogFormVisible = false
@@ -253,10 +254,10 @@
       updateData() {
         this.$refs['dataForm'].validate(valid => {
           if (valid) {
-            updateBrand(this.dataForm)
+            updateManufacturer(this.dataForm)
               .then(() => {
                 for (const v of this.list) {
-                  if (v.id === this.dataForm.id) {
+                  if (v.id === this.dataForm.manId) {
                     const index = this.list.indexOf(v)
                     this.list.splice(index, 1, this.dataForm)
                     break
@@ -278,7 +279,7 @@
         })
       },
       handleDelete(row) {
-        deleteBrand(row)
+        deleteManufacturer(row)
           .then(response => {
             this.$notify.success({
               title: 'Success',
@@ -299,18 +300,18 @@
         import('@/vendor/Export2Excel').then(excel => {
           const tHeader = [
             'brand_ID',
-            'company_ID',
-            'name_en',
-            'name_chn',
+            'manufacturer_ID',
+            'nameEn',
+            'nameCn',
             'Desc',
-            'img_id'
+            'picUrl'
           ]
-          const filterVal = ['brd_id','man_id', 'name_en','name_chn', 'desc', 'img_id']
+          const filterVal = ['manId','manId', 'nameEn','nameCn', 'manDesc', 'picUrl']
           excel.export_json_to_excel2(
             tHeader,
             this.list,
             filterVal,
-            'bran'
+            'manufacturer'
           )
           this.downloadLoading = false
         })
@@ -318,3 +319,29 @@
     }
   }
 </script>
+
+<style scoped>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #20a0ff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 120px;
+    height: 120px;
+    line-height: 120px;
+    text-align: center;
+  }
+  .avatar {
+    width: 145px;
+    height: 145px;
+    display: block;
+  }
+</style>
